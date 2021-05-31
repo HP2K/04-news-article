dd7b97246410396887f33034ce687b5be129e802
Hans Pelle Jart <hanspellejart@gmail.com>
2021-05-31 19:48:27 +0200

    api

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const api_key = process.env.API_KEY;
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
