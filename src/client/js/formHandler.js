8a66295f0f1f89275bfae20422469d93f3729fca
Hans Pelle Jart <hanspellejart@gmail.com>
2021-06-04 12:59:00 +0200

    post route

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,30 +1,43 @@
+
+const apiBaseUrl = "https://api.meaningcloud.com/sentiment-2.1";
+const api_key = process.env.API_KEY;
+
+const postResults = async (url = "http://localhost:8081/test", data = {}) => {
+    console.log("::: Form Submitted :::", data);
+    const response  = await fetch ("http://localhost:8081/test", {
+    method: 'POST',
+    credentials: 'same-origin',
+    mode: 'cors'
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify( {'url':url})
+    });
+    try{
+        const newData = await response.json();
+        return newData;
+        }    catch (error) {
+        console.log("error", error);
+    }
+ };
+
 function handleSubmit(event) {
     event.preventDefault()
-}
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-
-    Client.checkForName(formText)
-
-    console.log("::: Form Submitted :::")
-    
-    if (Client.checkForName(formText)) {
-        fetch('http://localhost:8081/test', {
-            method: 'POST',
-            credentials: 'same-origin',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        })
-    
-    .then(res => {
-        return res.json()
-    
+    if (checkForurl(formText)) {
+    postResults(formText)
+    .then(res=> {
+        document.getElementsById('score_tag').innerHTML = `score_tag: $(res.score_tag)`;
+        document.getElementsById('model').innerHTML = `Model: ${res.model}`;
+        document.getElementsById('agreement').innerHTML = `Agreement: ${res.agreement}`; 
+        document.getElementsById('subjectivity').innerHTML = `Subjectivity: ${res.subjectivity}`;
+        document.getElementsById('confidence').innerHTML = `Confidence: ${res.confidence}`;
+        document.getElementsById('irony').innerHTML = `Irony: ${res.irony}`;
+     
     })
-    .then(function(data) {
-        document.getElementsByClassName('results').innerHTML = data.message
-    })
+}else {
+    alert("Enter correct url")
 }
 
-export { handleSubmit }
+export { handleSubmit }}
